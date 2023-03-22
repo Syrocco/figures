98a16f00eb0f39d90c73e0af85325d1a6418b69b
Syrocco <maire.raphael@yahoo.fr>
2023-03-22 15:42:02 +0100

    :(

--- delta model example.py
+++ delta model example.py
@@ -24,11 +24,14 @@ phi = np.loadtxt(r"delta model example/phi.txt")
 E1 = np.loadtxt(r"delta model example/E1.txt")
 E3 = np.loadtxt(r"delta model example/E3.txt")
 E2 = np.loadtxt(r"delta model example/E2.txt")
-
+E4 = np.loadtxt(r"delta model example/E4.txt")
+E5 = np.loadtxt(r"delta model example/E5.txt")
 
 ME1 = np.array([np.mean(E[-100:]) for E in E1])
 ME2 = np.array([np.mean(E[-100:]) for E in E2])
 ME3 = np.array([np.mean(E[-100:]) for E in E3])
+ME4 = np.array([np.mean(E[-100:]) for E in E4])
+ME5 = np.array([np.mean(E[-100:]) for E in E5])
 
 
 plt.scatter(phi, ME3/tempMax(0.95, 0.025), label = r"$\Delta/\beta = 2.5$", s = 7)
@@ -47,6 +50,18 @@ time3 = np.array([curve_fit(partial(exp, maxim = E3[i][0], minim = np.mean(E3[i]
 plt.scatter(phi, time3*0.01, label = r"$\Delta/\beta = 2.5$", s = 7)
 plt.scatter(phi, time1*0.01, label = r"$\Delta/\beta = 3.0$", marker = "x", s = 7)
 plt.scatter(phi, time2*0.01, label = r"$\Delta/\beta = 3.5$", marker = "s", s = 7)
+plt.xlabel(r"$\phi$")
+plt.ylabel(r"$\chi\gamma$")
+plt.legend()
+plt.grid()
+
+plt.figure()
+time4 = np.array([curve_fit(partial(exp, maxim = E4[i][0], minim = np.mean(E1[i][-100:])), t[:300], E4[i][:300])[0] for i in range(300)])
+time5 = np.array([curve_fit(partial(exp, maxim = E5[i][0], minim = np.mean(E2[i][-100:])), t[:300], E5[i][:300])[0] for i in range(300)])
+
+plt.scatter(phi, time4*0.005, label = r"$\gamma = 0.005$", s = 7)
+plt.scatter(phi, time1*0.01, label = r"$\gamma = 0.01$", marker = "x", s = 7)
+plt.scatter(phi, time5*0.02, label = r"$\gamma = 0.02$", marker = "s", s = 7)
 plt.xlabel(r"$\phi$")
 plt.ylabel(r"$\chi\gamma$")
 plt.legend()
@@ -79,6 +94,9 @@ plt.legend()
 plt.grid()
 plt.title(r"$\phi_c$ = critical packing fraction obtained from max of time scale")
 
+
+
+
 """
 def sortAccordingToA(A, *args):
     arg = np.argsort(A)
@@ -102,4 +120,18 @@ E3 = np.array([data.E for data in allData3])
 
 sortAccordingToA(phi1, E1)
 sortAccordingToA(phi2, E2)
-sortAccordingToA(phi3, E3)"""
+sortAccordingToA(phi3, E3)
+
+allData2 = dataArray("/home/syrocco/Documents/absorbing/Data/simple delta/0.03 g=0.005/")
+allData3 = dataArray("/home/syrocco/Documents/absorbing/Data/simple delta/0.03 g=0.02/")
+
+phi2 = np.array([data.phi for data in allData2])
+E2 = np.array([data.E for data in allData2])
+phi3 = np.array([data.phi for data in allData3])
+E3 = np.array([data.E for data in allData3])
+
+sortAccordingToA(phi2, E2)
+sortAccordingToA(phi3, E3)
+
+np.savetxt("delta model example/E4.txt", E2)
+np.savetxt("delta model example/E5.txt", E3)"""
